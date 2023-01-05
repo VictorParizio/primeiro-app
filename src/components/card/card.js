307c11b719551af25b768faef59a9c0aad4ad407
Victor Parizio <victorparizio@hotmail.com>
2023-01-05 17:32:19 -0300

    prop children

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,24 +1,10 @@
 import React from 'react'
 import './card.css'
 
-/*
-class Card extends React.Component {
-    render(){
-        return(
-            <div className='card'>
-                <h3>{this.props.title}</h3>
-                <p>esse é o texto do carde</p>
-            </div>
-        )
-    }
-}
-*/
-
-const Card = ({title}) => {
+const Card = ({children}) => {
     return (
         <div className='card'>
-            <h3>{title}</h3>
-            <p>esse é o texto do card</p>
+            {children}
         </div>
     )
 }
