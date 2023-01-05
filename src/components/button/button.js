393d354523216be7f6c5ccbba1bebd12ca08f34c
Victor Parizio <victorparizio@hotmail.com>
2023-01-05 19:22:23 -0300

    Propriedades default de componentes

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,7 +1,19 @@
+import React, { Component } from 'react'
 import './button.css'
+/*
+const Button = (props) => {
+    return <button className="btn">{props.label}</button>
+}
+*/
 
-const Button = () => {
-    return <button className="btn">Clique aqui</button>
+class Button extends Component {
+    render(){
+    return <button className="btn">{this.props.label}</button>
+    }
+}
+
+Button.defaultProps = {
+    label: 'Clique aqui'
 }
 
 export default Button
