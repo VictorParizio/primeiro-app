ae8b6e380a7ca821ea9e9035954afd97938820ed
Victor Parizio <victorparizio@hotmail.com>
2023-01-05 19:31:33 -0300

    Utilizando múltiplos componentes (aninhando componentes)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Button from './components/button/button'
+import Cards from './components/cards/cards'
 
 function App() {
   return (
     <>
-      <Button label='Baixar CV' />
-
-      <Button />
+      <Cards />
     </>
   );
 }
