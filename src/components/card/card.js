d74c20183f0ca27b97aa5d39cd9fd5d4294abc90
Victor Parizio <victorparizio@hotmail.com>
2023-01-05 17:19:16 -0300

    Props passando propriedades para os componentes

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import './card.css'
 
+/*
 class Card extends React.Component {
     render(){
         return(
             <div className='card'>
-                <h3>Título</h3>
+                <h3>{this.props.title}</h3>
                 <p>esse é o texto do carde</p>
             </div>
         )
     }
+}
+*/
+
+const Card = ({title}) => {
+    return (
+        <div className='card'>
+            <h3>{title}</h3>
+            <p>esse é o texto do card</p>
+        </div>
+    )
 }
 
 export default Card
